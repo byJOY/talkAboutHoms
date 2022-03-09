fa74a9da6eb5e2cd7c860a823983ceec6b343f43
talkAboutHoms <work.jhlee@gmail.com>
2022-03-09 19:31:10 +0900

    프로젝트 시작합니다

---
+++ src/main/java/com/jjj/talkAboutHomes/TalkAboutHomesApplication.java
@@ -0,0 +1,13 @@
+package com.jjj.talkAboutHomes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalkAboutHomesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalkAboutHomesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jjj/talkAboutHomes/TalkAboutHomesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jjj.talkAboutHomes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalkAboutHomesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
